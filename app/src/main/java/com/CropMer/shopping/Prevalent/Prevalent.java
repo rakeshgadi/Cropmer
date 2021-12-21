d82aa07be7aa23db93cd4b273916fcae474af7fb
gadirakesh <gadirakesh2000@gmail.com>
2021-12-21 13:10:34 +0530

    Before Updates

---
+++ app/src/main/java/com/CropMer/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.CropMer.shopping.Prevalent;
+
+import com.CropMer.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
